92eae878d89e6d6c55281343034e911fde639008
Ola Ajibode <ola.ajibode@gmail.com>
2017-01-25 17:50:03 +0000

    Create config in etc default

---
+++ config_in_default.sh
@@ -0,0 +1,16 @@
+# Remember you're on Ubuntu Ola!
+
+START_DAEMON=true
+ES_USER=elasticsearch
+ES_GROUP=elasticsearch
+LOG_DIR=/var/log/elasticsearch
+DATA_DIR=/var/lib/elasticsearch
+WORK_DIR=/tmp/elasticsearch
+CONF_DIR=/etc/elasticsearch
+CONF_FILE=/etc/elasticsearch/elasticsearch.yml
+RESTART_ON_UPGRADE=true
+
+
+chown -R elasticsearch:elasticsearch /var/lib/elasticsearch/
+
+
