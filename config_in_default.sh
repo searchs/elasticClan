a8864f8f7bcdac0a0af7616a6eae1a7fa9c72974
Ola Ajibode <ola.ajibode@gmail.com>
2017-02-03 19:00:21 +0000

    conf_file is no longer used

--- config_in_default.sh
+++ config_in_default.sh
@@ -8,7 +8,6 @@ LOG_DIR=/var/log/elasticsearch
 DATA_DIR=/var/lib/elasticsearch
 WORK_DIR=/tmp/elasticsearch
 CONF_DIR=/etc/elasticsearch
-CONF_FILE=/etc/elasticsearch/elasticsearch.yml
 RESTART_ON_UPGRADE=true
 
 
