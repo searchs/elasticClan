5a299d075f8cb6c1db35263f3f5d3b4d4e82ad42
Ola Ajibode <searchs@users.noreply.github.com>
2017-03-15 10:12:24 +0000

    remove unused code

--- deals_loader.py
+++ deals_loader.py
@@ -116,30 +116,3 @@ curl -XPUT 'localhost:9200/markets/product/1?pretty' -d'
     "product_GTIN": "",
     "basket_link": ""
 }'
-#
-# def get_max_profit(stock_prices_yesterday):
-#
-#   max_profit = 0
-#
-#   # go through every time
-#   for outer_time in xrange(len(stock_prices_yesterday)):
-#
-#       # for every time, go through every OTHER time
-#       for inner_time in xrange(len(stock_prices_yesterday)):
-#
-#           # for each pair, find the earlier and later times
-#           earlier_time = min(outer_time, inner_time)
-#           later_time   = max(outer_time, inner_time)
-#
-#           # and use those to find the earlier and later prices
-#           earlier_price = stock_prices_yesterday[earlier_time]
-#           later_price   = stock_prices_yesterday[later_time]
-#
-#           # see what our profit would be if we bought at the
-#           # earlier price and sold at the later price
-#           potential_profit = later_price - earlier_price
-#
-#           # update max_profit if we can do better
-#           max_profit = max(max_profit, potential_profit)
-#
-#   return max_profit
